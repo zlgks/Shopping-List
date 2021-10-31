621b2cab543c07c1977aff6c53502ef32024aa9d
zlgks <dhzlgks@naver.com>
2021-10-31 16:45:10 +0900

    Shopping-List : delete all button

--- main.js
+++ main.js
@@ -1,101 +1,81 @@
 'use strict';
 
-// const input = document.querySelector('.input');
-// const button = document.querySelector('.button');
-// const main = document.querySelector('.main');
-
-// button.addEventListener("click", (event) => {
-//     const target = event.target.parentNode;
-//     const value = input.value;
-//     if(event.target !== button) {
-//         addList(value);
-//         input.value = null;
-//     }
-// })
-
-// input.addEventListener("keypress", (event) => {
-//     const value = input.value;
-//     if(event.key == 'Enter'){
-//         addList(value);
-//         console.log(input.value)
-//         input.value = null
-//     }
-// })
-
-// main.addEventListener("click", (event) => {
-//     if(event.target.dataset.delate == 'trash') {
-//         const list = event.target.parentNode;
-//         list.remove();
-//     }
-// })
-
-
-// function addList(input) {
-//     const list = document.createElement('list');
-//     const span = document.createElement('span');
-//     const trash = document.createElement('i');
-//     list.setAttribute('class', 'list');
-//     span.setAttribute('class', 'list__value');
-//     span.textContent = input;
-//     trash.setAttribute('class', 'fas fa-trash-alt');
-//     trash.setAttribute('data-delate', 'trash');
-//     main.append(list);
-//     list.append(span);
-//     list.append(trash);
-
-// }
-
-
-
+const section = document.querySelector('.section');
 const input = document.querySelector('.footer__input');
 const addBtn = document.querySelector('.footer__button');
 const main = document.querySelector('.main');
+const allDeleteBtn = document.querySelector('.all-delete');
 
-function addItem(inputValue) {
-    if(inputValue == '') {
-        return;
+let count = 0;
+
+function allDeleteBtnView() {
+    if(main.childElementCount > 0) {
+        allDeleteBtn.style.opacity = 1;
+        allDeleteBtn.style.cursor = "pointer";
     } else {
-    const li = document.createElement('li');
-    li.setAttribute('class','main__item');
-    const span = document.createElement('span');
-    span.setAttribute('class','main__item__name')
-    const btn = document.createElement('button');
-    btn.setAttribute('class','main__item__delete');
-
-    span.textContent = inputValue;
-    btn.innerHTML = `<i class="fas fa-trash-alt"></i>`
-
-
-    li.append(span);
-    li.append(btn)
-    main.append(li);
-
-    input.value = '';
-    input.focus();
-
-    li.scrollIntoView();
-
-    // return li;
+        allDeleteBtn.style.opacity = 0;
+        allDeleteBtn.style.cursor = "none";
 
     }
 }
 
+function addList() {
+    if(input.value == '') {
+        return;
+    } else {
+        
+        const item = addItem(input.value);
+        main.append(item);
+
+        input.value = '';
+        input.focus();
+        item.scrollIntoView();
+        allDeleteBtnView();
+}
+
+
+function addItem(inputValue) {
+    const li = document.createElement('li');
+    li.setAttribute('class','main__item');
+    li.setAttribute('data-count',`${count}`);
+    li.innerHTML = `<span class="main__item__name">
+                        ${inputValue}
+                    </span>
+                    <button class="main__item__delete">
+                        <i class="fas fa-trash-alt" data-type="delete" data-count="${count}"></i>
+                    </button>`
+    count++;
+    return li;
+    }
+}
+
 addBtn.addEventListener('click', () => {
-    addItem(input.value);
-    
+    addList();
 })
 
 input.addEventListener('keypress', (e) => {
     if(e.key == 'Enter') {
-        addItem(input.value);
+        addList();
     }
 })
 
-main.addEventListener('click', (event) => {
-    if(event.target.tagName == 'I') {
-        const target = event.target.parentNode;
-        target.parentNode.remove();
-    } else if(event.target.tagName == 'BUTTON'){
-        event.target.parentNode.remove();
+section.addEventListener('click', (event)=> {
+    const type = event.target.dataset.type;
+    switch(type) {
+        case 'delete':
+            const dataCount = event.target.dataset.count;
+            const deleteList = document.querySelector(`.main__item[data-count="${dataCount}"`);
+            deleteList.remove();
+            allDeleteBtnView();
+            break;
+        
+        case 'all':
+            main.innerHTML = '';
+            allDeleteBtnView();
     }
+    // if(type === 'delete') {
+    //     const dataCount = event.target.dataset.count;
+    //     const deleteList = document.querySelector(`.main__item[data-count="${dataCount}"`);
+    //     deleteList.remove();
+    // } else if ()
 })
